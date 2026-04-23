2990b78ee3fa220991e2018d93d42bfced7260b0
FluidNatalie <nateandgeorge@gmail.com>
2026-04-23 13:43:57 +0200

    Spacehaul 0.0.1 - First Github Release

---
+++ data-updates.lua
@@ -0,0 +1,2 @@
+-- Factoriopedia (barrel) entries are hidden here
+require("__Spacehaul__.space_age_files.UPDATE_factoriopedia")
---
+++ data.lua
@@ -0,0 +1,5 @@
+-- Factoriopedia entries are hidden here
+require("__Spacehaul__.space_age_files.factoriopedia")
+require("__Spacehaul__.space_age_files.alternative_recipes")
+-- Data File
+require("__Spacehaul__.space_age_files.vanilla_data")
